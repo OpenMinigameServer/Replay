13050407cef0cb81c27c2ba1c353828dc316fca6
NickAc <32451103+NickAcPT@users.noreply.github.com>
2021-01-25 04:26:14 +0000

    Only save non default values

--- model/src/main/kotlin/io/github/openminigameserver/replay/model/recordable/RecordablePosition.kt
+++ model/src/main/kotlin/io/github/openminigameserver/replay/model/recordable/RecordablePosition.kt
@@ -1,3 +1,12 @@
 package io.github.openminigameserver.replay.model.recordable
 
-data class RecordablePosition(val x: Float, val y: Float, val z: Float, val yaw: Float, val pitch: Float)
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+data class RecordablePosition(
+    val x: Float = 0f,
+    val y: Float = 0f,
+    val z: Float = 0f,
+    val yaw: Float = 0f,
+    val pitch: Float = 0f
+)
